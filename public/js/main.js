956fecc3dc7b24849308a8f89bdcd4c7dd419c65
Alex Normand <anormnd@gmail.com>
2012-10-07 00:41:37 +0200

    added require.js

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+require({
+    paths: {
+        backbone: 'libs/backbone',
+        jquery: 'libs/jquery',
+        cs: 'libs/require/cs',
+        'coffee-script': 'libs/coffee-script'
+    }
+}, ['cs!csmain']);
+
